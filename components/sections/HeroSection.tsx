c4dd44cd9349735852787e57c56debf8a5f16d69
KilJae69 <adi.toromanovic@outlook.com>
2025-03-26 16:28:19 +0100

    .

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,13 +1,31 @@
 import Image from "next/image";
 import { Container } from "../shared/Container";
 import PrimaryButton from "../shared/PrimaryButton";
-import GradientBackground from "../shared/gradient-background";
+
 import { FadeIn, FadeInStagger } from "../shared/FadeIn";
 
 export default function HeroSection() {
   return (
-    <section className="">
-      <GradientBackground  className="relative ">
+    <section className="relative">
+       <div
+        className="absolute w-[500px] h-[500px] md:w-[800px] md:h-[800px] rounded-full opacity-30 md:opacity-40 blur-3xl 
+                  md:left-[-200px] md:top-1/4 
+                  left-1/2 top-[-100px] transform md:translate-y-0 -translate-x-1/2"
+        style={{
+          background: `radial-gradient(circle, #7c3aed 0%, transparent 70%)`,
+        }}
+      />
+
+      {/* Right gradient on large screens, bottom gradient on small screens */}
+      <div
+        className="absolute w-[500px] h-[500px] md:w-[800px] md:h-[800px] rounded-full opacity-30 md:opacity-40 blur-3xl 
+                  md:right-[-500px] md:bottom-1/4 md:left-auto
+                  left-1/2 bottom-[-100px] transform md:translate-y-0 -translate-x-1/2"
+        style={{
+          background: `radial-gradient(circle, #c4b5fd 0%, transparent 70%)`,
+        }}
+      />
+     
         <Container className=" pt-32 lg:pt-46">
           <FadeInStagger className="flex flex-col lg:flex-row">
             <FadeIn direction="right" className="flex flex-col gap-4 lg:min-w-1/2 ">
@@ -33,7 +51,7 @@ export default function HeroSection() {
             </FadeIn>
           </FadeInStagger>
         </Container>
-      </GradientBackground>
+      
     </section>
   );
 }
