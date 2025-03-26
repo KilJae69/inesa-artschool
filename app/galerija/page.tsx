cc5c6b3621c70b4d378bf92d4fddb171a807ee22
KilJae69 <adi.toromanovic@outlook.com>
2025-03-26 16:17:45 +0100

    .

--- app/galerija/page.tsx
+++ app/galerija/page.tsx
@@ -1,24 +1,24 @@
-
-
 import Gallery from "@/components/gallery/Gallery";
 
 import { Container } from "@/components/shared/Container";
 import PageIntro from "@/components/shared/PageIntro";
 
 import { galleryData } from "@/constants/galleryData";
+import { Suspense } from "react";
 // import { LayoutGrid } from "@/components/ui/layout-grid";
 
 export default function GalerijaPage() {
-
   return (
     <>
-    <PageIntro title="Galerija Slika"/>
-    <section className=" py-20 w-full">
-      {/* <LayoutGrid cards={cards} /> */}
-      <Container>
-        <Gallery galleryData = {galleryData}/>
-      </Container>
-    </section>
+      <PageIntro title="Galerija Slika" />
+      <section className=" py-20 w-full">
+        {/* <LayoutGrid cards={cards} /> */}
+        <Container>
+          <Suspense fallback={<div>Loading gallery...</div>}>
+            <Gallery galleryData={galleryData} />
+          </Suspense>
+        </Container>
+      </section>
     </>
   );
 }
